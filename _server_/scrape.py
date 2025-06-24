e51173b22b58d44482f6735fd99f876cd28ea3da
Przemek B <pprzemek89@gmail.com>
2025-06-25 00:01:47 +0200

    optymalizacja, przygotowanie pod multithreading

--- _server_/scrape.py
+++ _server_/scrape.py
@@ -1,83 +1,101 @@
 import requests
-import re
 from bs4 import BeautifulSoup
+import re
 from datetime import datetime
+import threading
+
+
+def result_processing(lock, results, link):
+    response = requests.get(link)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    data_segments = soup.find_all('table', class_='Napisy')
+    if len(data_segments) <= 1:
+        return False
+
+    # elementy html z napisami
+    for s in data_segments:
+        sub_id = s.find('input', attrs={'name': 'id'})
+        if not sub_id or sub_id in results:
+            continue
+
+        sub_id = sub_id['value']
+        title = s.find('td').get_text()
+        author_id = s.find('a')['href'][13:]
+        key = title + "_" + author_id
+
+        # sprawdzanie czy warto dalej zbierać dane
+        NEW_KEY = key not in results
+        NEW_ID = not NEW_KEY and any(
+            sr['id'] == sub_id for sr in results[key]['sub_results']
+        )
+        if not (NEW_KEY or NEW_ID):
+            continue
+
+        author_txt = s.find('a').get_text()[1:]
+        date = datetime.strptime(s.find_all('td')[1].get_text(), "%Y.%m.%d")
+        title_en = s.find_all('tr')[1].find('td').get_text()
+        episodes = []
+
+        # jeśli to serial a nie film
+        if re.search(r' ep\d+(?:-\d+)?$', title):
+            # odzielenie tytułu od nr. odcinka
+            ep_pos = title.rfind("ep")
+            episode = title[ep_pos + 2 :]
+            title = title[: ep_pos - 1]
+            title_en = title_en[: title_en.rfind("ep") - 1]
+
+            # '-' oznacza że w tym wyniku jest kilka odcinków
+            if '-' in episode:
+                episode, episode_max = episode.split('-')
+                for i in range(int(episode), int(episode_max) + 1):
+                    episodes.append(i)
+            else:
+                episodes = [int(episode)]
+
+        # lock żeby wątki nie zapisywały w tym samym czasie
+        with lock:
+            if NEW_KEY:
+                results[key] = {
+                    'sub_results': [],
+                    'title': title,
+                    'title_en': title_en,
+                    'author_id': author_id,
+                    'author_txt': author_txt,
+                }
+            if NEW_ID:
+                results[key]['sub_results'].append(
+                    {
+                        'id': sub_id,
+                        'episodes': episodes,
+                        'date': date,
+                    }
+                )
+    return True
 
 
 def search(txt):
     txt = txt.replace(" ", "+")
+    lock = threading.Lock()
     results = {}
 
     # pobieranie wyników z animesub.info, zapis według autora i nazwy anime
     for TITLE in ['org', 'en', 'pl']:  # każda wersja tytułu
         page = 0
-        while True:
-            response = requests.get(
-                f'http://animesub.info/szukaj.php?szukane={txt}&pTitle={TITLE}&od={page}'
-            )
+        while result_processing(
+            lock,
+            results,
+            f'http://animesub.info/szukaj.php?szukane={txt}&pTitle={TITLE}&od={page}',
+        ):
             page += 1
-            soup = BeautifulSoup(response.content, 'html.parser')
-            data_segments = soup.find_all('table', class_='Napisy')
-            if len(data_segments) <= 1:
-                break
-
-            for s in data_segments:
-                sub_id = s.find('input', attrs={'name': 'id'})
-                if not sub_id or sub_id in results:
-                    continue
-
-                sub_id = sub_id['value']
-                author_id = s.find('a')['href'][13:]
-                author_txt = s.find('a').get_text()[1:]
-                date = datetime.strptime(s.find_all('td')[1].get_text(), "%Y.%m.%d")
-
-                episode = s.find('td').get_text()
-                episode_max = False
-                episodes = []
-
-                # regex sprawdza czy to odcinek serialu czy film
-                if re.search(r' ep\d+(?:-\d+)?$', episode):
-                    ep_pos = episode.rfind("ep")
-                    title = episode[: ep_pos - 1]
-                    episode = episode[ep_pos + 2 :]
-
-                    # '-' oznacza że w tym wyniku jest kilka odcinków
-                    if '-' in episode:
-                        episode, episode_max = episode.split('-')
-                        episode_max = int(episode_max)
-                        for i in range(int(episode), episode_max + 1):
-                            episodes.append(i)
-                    else:
-                        episodes = [int(episode)]
-                else:
-                    title = episode
-                    episode = False
-
-                key = title + "_" + author_id
-                if key not in results:
-                    results[key] = {
-                        'sub_results': [],
-                        'title': title,
-                        'author_id': author_id,
-                        'author_txt': author_txt,
-                    }
-                if not any(sr['id'] == sub_id for sr in results[key]['sub_results']):
-                    results[key]['sub_results'].append(
-                        {
-                            'id': sub_id,
-                            'episodes': episodes,
-                            'date': date,
-                        }
-                    )
 
     batches = []
 
     def add_info(b, r):
         b['title'] = r['title']
+        b['title_en'] = r['title_en']
         b['author'] = r['author_txt']
         b['author_id'] = r['author_id']
         return b
-
 
     for key in results:
         r = results[key]
@@ -97,12 +115,3 @@ def search(txt):
         batches.append(add_info(batch, r))
 
     return batches
-
-
-if __name__ == '__main__':
-    from pprint import pprint
-
-    result = search("steins gate")
-    for g in result:
-        pprint(g)
-        print("\n\n\n")
