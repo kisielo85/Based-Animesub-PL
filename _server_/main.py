b81008aff0ce2c59a0969f889a68a4a132b531a1
Przemek B <pprzemek89@gmail.com>
2025-06-24 21:30:55 +0200

    czyszczenie kodu

--- _server_/main.py
+++ _server_/main.py
@@ -1,13 +1,15 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
+from http.server import ThreadingHTTPServer, BaseHTTPRequestHandler
 import json
 import scrape
 
+
 class handler(BaseHTTPRequestHandler):
-    def _set_cors_headers(self):
+    def _set_headers(self):
         self.send_header('Access-Control-Allow-Origin', '*')
         self.send_header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
         self.send_header('Access-Control-Allow-Headers', 'Content-Type')
-    
+        self.end_headers()
+
     def do_GET(self):
         if self.path == "/download":
             with open("cache/test.zip", "rb") as f:
@@ -15,36 +17,33 @@ class handler(BaseHTTPRequestHandler):
 
             self.send_response(200)
             self.send_header('Content-Disposition', 'attachment; filename="file.zip"')
-            self._set_cors_headers()
-            self.end_headers()
+            self._set_headers()
             self.wfile.write(content)
 
     def do_POST(self):
-        print("AUU")
-        if self.path == "/search":
-            print("AUUGH")
-            content_length = int(self.headers.get('Content-Length', 0))
-            body = self.rfile.read(content_length)
+        match self.path:
+            case "/search":
+                content_length = int(self.headers.get('Content-Length', 0))
+                body = self.rfile.read(content_length)
 
-            try:
-                data = json.loads(body)
-                title = data.get("title")
-                print("Odebrany title:", title)
-            except:
-                pass
+                try:
+                    data = json.loads(body)
+                    title = data.get("title")
+                except:
+                    self.send_response(400)
+                    self._set_headers()
+                    return
 
-            self.send_response(200)
-            self._set_cors_headers()
-            self.end_headers()
-            response=json.dumps(scrape.search(title))
-            self.wfile.write(response.encode('utf-8'))
-    
+                self.send_response(200)
+                self._set_headers()
+                response = json.dumps(scrape.search(title))
+                self.wfile.write(response.encode('utf-8'))
+
     def do_OPTIONS(self):
         self.send_response(200)
-        self._set_cors_headers()
-        self.end_headers()
+        self._set_headers()
 
 
 if __name__ == '__main__':
-    server = HTTPServer(('0.0.0.0', 8986), handler)
+    server = ThreadingHTTPServer(('0.0.0.0', 8986), handler)
     server.serve_forever()
