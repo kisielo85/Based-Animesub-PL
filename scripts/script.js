2e447b0a6b254e1bb407670bc13e94f278ac7c49
kamilek7 <kamijar20031@gmail.com>
2025-06-25 15:32:04 +0200

    styles

--- scripts/script.js
+++ scripts/script.js
@@ -23,7 +23,6 @@ async function search() {
         body: JSON.stringify({ title: (anime.toString()) })
     })
     data = await data.json(); // parsuj JSON
-    console.log(data);
     let len = data.length;
     let inside = "";
     for (let i=0; i<len; i++)
@@ -32,28 +31,38 @@ async function search() {
         let test = data[i].episodes.sort(function(a, b){return a-b});
         test.push(0);
         let testLen = test.length;
-        let first = test[0];	
-        for (let i=0;i<testLen; i++ )
+        let first = test[0];
+        if (testLen>1)
         {
-            if (i>0)
-                if (test[i] != test[i-1]+1)
-                {
-                    if (test[i-1]!=first)
-                        episodes.push(`${first}-${test[i-1]}`);				
-                    else
-                        episodes.push(`${first}`);
-                    first = test[i];	
-                                    
-                }
+            for (let i=0;i<testLen; i++ )
+            {
+                if (i>0)
+                    if (test[i] != test[i-1]+1)
+                    {
+                        if (test[i-1]!=first)
+                            episodes.push(`${first}-${test[i-1]}`);				
+                        else
+                            episodes.push(`${first}`);
+                        first = test[i];	
+                                        
+                    }
+            }	
+            episodes = episodes.join(", ");
         }	
-        console.log(episodes);
-        episodes = episodes.join(", ");
+        else
+            episodes="Film/OVA";
+        
         inside += `<div class='result'>
-                    <div class='name'>${data[i].title}</div>
+                    <div class='name'>${data[i].title_en}</div>
+                    <div class='subName'>${data[i].title}</div>
                     <div class='odcinki'>Dostępne odcinki: ${episodes}</div>
-                    <div class='autor'>Autor: <a href='http://animesub.info/osoba.php?id=${data[i].author_id}'>${data[i].author}</a></div>
+                    <div class='bottomRow'>
+                        <div class='autor'>Autor: <a href='http://animesub.info/osoba.php?id=${data[i].author_id}'>${data[i].author}</a></div>
+                        <div class='data'>${data[i].date}</div>
+                    </div>
                 </div>`
     }
+    console.log(data);
     document.getElementById('results').innerHTML = inside;
         
 }
