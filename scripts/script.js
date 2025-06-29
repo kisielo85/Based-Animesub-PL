943958ff419bedcb1f4eb432fa2922645e6f8f14
kamilek7 <kamijar20031@gmail.com>
2025-06-29 16:14:52 +0200

    progress bar

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,41 @@
-const api_link = "https://basedanimesub.153070065.xyz"
-//const api_link = "http://localhost:8986"
+//const api_link = "https://basedanimesub.153070065.xyz"
+const api_link = "http://localhost:8986"
 
 const delay = ms => new Promise(res => setTimeout(res, ms));
+
+var IDsMap = new Map();
+
+function getDivID(ids)
+{
+    return ids.join("-");
+}
+
+
+async function checkDownloadProgress(id)
+{
+    
+    progress = 0
+    divId = IDsMap.get(id).toString();
+    element =document.getElementsByName(divId)[0];
+    element2 =document.getElementsByName(`${divId}-m`)[0];
+    element.style.opacity = '100%';
+    element2.style.opacity = '100%';
+    while (progress < 100) {
+        data = await fetch(`${api_link}/download_progress`, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ job_id: id })
+        })
+        data = await data.json()
+        progress = data['progress']
+        element.style.width= `${progress}%`;
+        console.log("progress:", progress)
+        await delay(500)
+    }
+    element.style.opacity = '0%';
+    element2.style.opacity = '0%';
+}
+
 
 async function download(ids) {
     // wysłanie id do pobrania, i odebranie job_id
@@ -12,22 +46,13 @@ async function download(ids) {
     })
     data = await data.json()
     const job_id = data['job_id']
+    let id = getDivID(ids);
+    IDsMap.set(job_id, id);
 
     console.log("serwer odpowiedział, job_id:", job_id)
 
     // sprawdzanie postępu w tworzeniu .zip
-    progress = 0
-    while (progress < 100) {
-        data = await fetch(`${api_link}/download_progress`, {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ job_id: job_id })
-        })
-        data = await data.json()
-        progress = data['progress']
-        console.log("progress:", progress)
-        await delay(500)
-    }
+    await checkDownloadProgress(job_id);
 
     // pobranie gotowego pliku
     data = await fetch(`${api_link}/download`, {
@@ -54,13 +79,15 @@ async function download(ids) {
     a.download = filename;
     a.click();
     URL.revokeObjectURL(url);
+    IDsMap.delete(job_id);
 }
 
 async function search(e) {
+    
     e.preventDefault();
     let anime = document.getElementById("anime").value;
-    console.log("search:", anime)
-
+    console.log("search:", anime);
+    // pobieranie danych z serwera
     data = await fetch(`${api_link}/search`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
@@ -92,12 +119,15 @@ async function search(e) {
             else
                 episodes = "Film/OVA";
 
+            divId = getDivID(data[i].sub_ids);
+            // wypelnienie diva odpowiednimi danymi
             inside +=
                 `<div class='resultDisabled'>
                     <div class='name'>${data[i].title_en}</div>
                     <div class='subName'>${data[i].title}</div>
                     <div class='odcinki'>Dostępne odcinki: ${episodes}</div>
                     <div class='down'><button onclick='download([${data[i].sub_ids}])'>Pobierz paczkę</button></div>
+                    <div class='progress-bar' name='${divId}-m'><div name='${divId}' class='inner-progres'></div></div>
                     <div class='bottomRow'>
                         <div class='autor'>Autor: <a href='http://animesub.info/osoba.php?id=${data[i].author_id}'>${data[i].author}</a></div>
                         <div class='data'>${data[i].date}</div>
